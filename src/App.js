c7a35d29c6fd9dbf9ec7aee6a8a0d92e6b440d43
Krushnna Baviskar <krushnnabaviskar@gmail.com>
2024-03-30 18:58:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <div className="developer">
                 <span>Developed by </span>
                 <a href="https://www.linkedin.com/in/krushnna" target="_blank" rel="noopener noreferrer">
-                    <img src="http://m.gettywallpapers.com/wp-content/uploads/2023/06/Roronoa-Zoro-Pfp-for-discord.jpg"
+                    <img src="/zoro.jpeg"
                          alt="Krushnna" />
                     krushnna
                 </a>
